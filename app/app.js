25b659a70b6293a3e5b42d811c788d9683d48481
Victorien Schweitzer <victorien-s@live.fr>
2016-01-08 11:44:04 +0100

    Ajout du token dans angular

--- app/app.js
+++ app/app.js
@@ -1,1 +1,4 @@
-var app = angular.module("slack", [])
+var app = angular.module("slack", []);
+app.config([function($http) {
+    $http.defaults.headers.common.Authorization = 'Token token=2654ccd286fb4250a7186d3e126f5b92';
+}])
